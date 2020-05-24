21c1df49b32c9f2af45f1dd5a6bb1cbdeeb82e21
AshfoMon <ashfordmonte@gmail.com>
2020-05-24 14:48:26 -0500

    Added scraping functionality for pcgamer.com, added Article db model, added landing page GET route and index handlebars to display

--- server.js
+++ server.js
@@ -23,6 +23,34 @@ app.set("view engine", "handlebars");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
+app.get("/", function(req, res) {
+  axios.get("https://www.pcgamer.com/news/").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    $("h3.article-name").each(function(i, element) {
+
+      var title = $(element).text();
+      var link = $(element).parent().parent().parent().parent().attr("href");
+      var synopsis = $(element).parent().parent().children("p").text().slice(5,-1);
+  
+      var result = {};
+
+      result.title = title;
+      result.link = link;
+      result.synopsis = synopsis;
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+    res.render("index");
+  });
+});
+
 // Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
