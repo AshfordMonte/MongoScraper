2fd0c0cebdbf9933c39590ad20e1a376a02405b8
AshfoMon <ashfordmonte@gmail.com>
2020-05-29 17:10:57 -0500

    Added fixed public folder for styling and front end js, added custom css for landing page

--- server.js
+++ server.js
@@ -3,22 +3,21 @@ const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const path = require("path");
 
 const db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
